1919cef635ab11de5b6cbf9adbba747505df41c3
Dorian Ferrer <dferrer333@gmail.com>
2020-09-15 18:33:09 -0600

    Removed Default Constructor, Made Factories Return Shared Pointers...

--- src/Matrix.h
+++ src/Matrix.h
@@ -2,14 +2,13 @@
 #define MATRIX_H
 
 #include <array>
+#include <memory>
 
 namespace LinearAlgebra {
   template <size_t width, size_t height>
   class Matrix {
     public:
-      // creates identity matrix of specified size
-      Matrix();
-      // constructs matrix with provided 2-D Array
+      Matrix() = delete;
       Matrix(const std::array<std::array<float, height>, width>& matrix);
       Matrix(const Matrix& other);
 
@@ -20,18 +19,18 @@ namespace LinearAlgebra {
   };
 
   template<size_t width, size_t height>
-  Matrix<width, height> createIdentityMatrix();
+  std::shared_ptr<Matrix<width, height>> createIdentityMatrix();
 
   template<size_t width, size_t height>
-  Matrix<width, height> createTranslationMatrix(
+  std::shared_ptr<Matrix<width, height>> createTranslationMatrix(
       float xTranslation, float yTranslation, float zTranslation);
 
   template<size_t width, size_t height>
-  Matrix<width, height> createRotationMatrix(
+  std::shared_ptr<Matrix<width, height>> createRotationMatrix(
       float xRotation, float yRotation, float zRotation);
 
   template<size_t width, size_t height>
-  Matrix<width, height> createScaleMatrix(
+  std::shared_ptr<Matrix<width, height>> createScaleMatrix(
       float xScale, float yScale, float zScale);
 }
 
