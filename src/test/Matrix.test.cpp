27d643f468b2ca27e35d23163fca60f6a108da66
Dorian Ferrer <dferrer333@gmail.com>
2020-09-16 10:32:58 -0600

    Completed First Matrix Test

--- src/test/Matrix.test.cpp
+++ src/test/Matrix.test.cpp
@@ -1,13 +1,26 @@
 #include <array>
+#include <cassert>
 #include <iostream>
 #include "Matrix.h"
 
+bool simple_matrix_construction();
+
 int main() {
-  std::cout << "Implement Matrix Test!!";
-
-  LinearAlgebra::Matrix<5, 5> myMatrix();
-
-  std::cout << &myMatrix << std::endl;
+  assert(simple_matrix_construction() == true);
 
   return 0;
+}
+
+bool simple_matrix_construction() {
+  try {
+    std::array<float, 2> row1 = {1, 2};
+    std::array<float, 2> row2 = {3, 4};
+    std::array<std::array<float, 2>, 2> matrix = {row1, row2};
+
+    LinearAlgebra::Matrix<2, 2> myMatrix(matrix);
+  } catch (std::exception &error) {
+    return false;
+  }
+
+  return true;
 }
