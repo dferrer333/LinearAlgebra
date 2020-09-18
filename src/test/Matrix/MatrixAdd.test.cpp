fad14591b9d778edc4d246ca98f5ec80b5774b4a
Dorian Ferrer <dferrer333@gmail.com>
2020-09-18 09:05:30 -0600

    (test) Unknown: Matrix Add Test Skeleton

--- src/test/Matrix/MatrixAdd.test.cpp
+++ src/test/Matrix/MatrixAdd.test.cpp
@@ -5,7 +5,16 @@ namespace Test {
     namespace Add {
       bool run_all_tests();
 
-      
+      bool should_not_add_matrices_without_columns();
+      bool should_not_add_matrices_without_rows();
+      bool should_not_add_matrices_with_varied_sizes();
+
+      bool should_add_two_uniform_matrices();
+      bool should_add_two_non_uniform_matrices();
+      bool should_add_two_matrices_forward_and_reverse();
+
+      bool should_add_two_single_element_matrices();
+      bool should_add_two_large_matrices();
     }
   }
 }
